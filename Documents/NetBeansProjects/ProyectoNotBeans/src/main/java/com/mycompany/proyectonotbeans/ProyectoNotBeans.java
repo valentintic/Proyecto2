80de07d2b74faeabb770f4ec0202cef1c99bd0e1
valentintic <valentinpreuteseitic@gmail.com>
2022-10-03 17:20:38 +0200

    ProyectoNetBeans

---
+++ Documents/NetBeansProjects/ProyectoNotBeans/src/main/java/com/mycompany/proyectonotbeans/ProyectoNotBeans.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectonotbeans;
+
+/**
+ *
+ * @author valentin
+ */
+public class ProyectoNotBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
